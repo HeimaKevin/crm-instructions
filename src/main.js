dc8706590aad25ff0b68a4ab9368c71cf771490b
ChungWenTing <cwtk159x37@gmail.com>
2024-02-05 09:46:49 +0800

    add: vxe-table套件 update: 初始設定 delete: 不必要檔案

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import VXETable from 'vxe-table'
 
-Vue.config.productionTip = false;
+import 'vxe-table/lib/style.css'
+import './assets/style/all.scss'
+
+Vue.use(VXETable)
+Vue.config.productionTip = false
 
 new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
